9a232b05d9b76eb163f2b29c21d72e3f40736e5d
udayan11-delima <udayanghadi1@gmail.com>
2025-12-05 14:48:59 +0530

    Create exp1.c

---
+++ exp1.c
@@ -0,0 +1,12 @@
+/*
+Name: Udayan Ghadigaonkar
+UIN:251P117
+Class: Comp Eng - D3
+*/
+#include <stdio.h>
+int main() {
+  printf("Name : Udayan Ghadigaonkar \nAge: 19\n"); 
+  printf("Class: F.E ,Div:D\n");
+  printf("UIN : 251P117");
+ return 0;
+}
