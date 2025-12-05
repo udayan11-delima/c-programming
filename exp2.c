eaed3965cd2749bc3f8a404292bc44542836eebb
udayan11-delima <udayanghadi1@gmail.com>
2025-12-05 14:46:32 +0530

    Create exp2.c

---
+++ exp2.c
@@ -0,0 +1,19 @@
+/*
+Name:Udayan Ghadigaonkar
+UIN:251P117       COMPUTER ENGINEERING - D3
+*/
+#include<stdio.h>
+int main(){
+     int num;
+     
+     printf("enter the num :");
+     scanf("%d",&num);
+    if(num %2==0){
+        printf("even");
+    }
+    else{
+        printf("odd");
+    }
+    
+     return 0;
+}
